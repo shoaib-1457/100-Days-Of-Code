19c5b36efd2cd79aba98936808eed4695a7138e3
Sheikh Mohd Shoaib <116951006+shoaib-1457@users.noreply.github.com>
2023-10-10 09:02:58 +0530

    Create Day8(Next Permutation).java

---
+++ Day8(Next Permutation).java
@@ -0,0 +1,40 @@
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int ind1=-1;
+        int ind2=-1;
+        for(int i=nums.length-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+                ind1=i;
+                break;
+            }
+        }
+        if(ind1==-1){
+            reverse(nums,0);
+        }
+        else{
+            for(int i=nums.length-1;i>=0;i--){
+                if(nums[i]>nums[ind1]){
+                    ind2=i;
+                    break;
+                }
+            }
+
+            swap(nums,ind1,ind2);
+            reverse(nums,ind1+1);
+        }
+    }
+    void swap(int[] nums,int i,int j){
+        int temp=nums[i];
+        nums[i]=nums[j];
+        nums[j]=temp;
+    }
+    void reverse(int[] nums,int start){
+        int i=start;
+        int j=nums.length-1;
+        while(i<j){
+            swap(nums,i,j);
+            i++;
+            j--;
+        }
+    }
+}
