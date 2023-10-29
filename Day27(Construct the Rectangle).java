fb1fa61f82cbf7aee89926a34b34e4b619405e82
Sheikh Mohd Shoaib <116951006+shoaib-1457@users.noreply.github.com>
2023-10-29 08:37:45 +0530

    Create Day27(Construct the Rectangle).java

---
+++ Day27(Construct the Rectangle).java
@@ -0,0 +1,10 @@
+class Solution {
+    public int[] constructRectangle(int area) {
+    for(int i = (int) Math.sqrt(area) ;i>=1; i--){
+        if(area % i == 0){
+        return new int [] {area/i, i};
+        }
+    }
+        return new int []{area, 1};
+    }
+}
