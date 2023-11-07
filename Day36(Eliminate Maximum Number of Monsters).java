8bda8bf7c679a99f64c091c5b00391ee3b406f51
Sheikh Mohd Shoaib <116951006+shoaib-1457@users.noreply.github.com>
2023-11-07 08:24:51 +0530

    Create Day36(Eliminate Maximum Number of Monsters).java

---
+++ Day36(Eliminate Maximum Number of Monsters).java
@@ -0,0 +1,16 @@
+class Solution {
+    public int eliminateMaximum(int[] dist, int[] speed) {
+        int n = dist.length;
+        double[] ans = new double[n];
+        for (int i = 0; i < n; i++) {
+            time[i] = (double)dist[i] / speed[i];
+        }
+        Arrays.sort(time);
+        for (int i = 0; i < n; i++) {
+            if (time[i] <= i) {
+                return i;
+            }
+        }
+        return n;
+    }
+}
