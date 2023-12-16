d5d15ee97b54a8d4442c17e5e64cbe1ecc2f7292
Sheikh Mohd Shoaib <116951006+shoaib-1457@users.noreply.github.com>
2023-12-16 07:03:48 +0530

    Create Day75(Valid Anagram).java

---
+++ Day75(Valid Anagram).java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        char[] s1 = s.toCharArray();
+        char[] s2 = t.toCharArray();
+
+        Arrays.sort(s1);
+        Arrays.sort(s2);
+
+        return Arrays.equals(s1,s2);
+    }
+}
