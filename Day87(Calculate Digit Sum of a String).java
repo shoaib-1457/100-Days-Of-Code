42900c7f227b94e8501bfaf533106be47a64b612
Sheikh Mohd Shoaib <116951006+shoaib-1457@users.noreply.github.com>
2023-12-28 09:03:51 +0530

    Update Day87(Calculate Digit Sum of a String).java

--- Day87(Calculate Digit Sum of a String).java
+++ Day87(Calculate Digit Sum of a String).java
@@ -1,17 +1,17 @@
 class Solution {
     public String digitSum(String s, int k) {
-        StringBuilder stb = new StringBuilder();
+        StringBuilder str = new StringBuilder();
         int ans = 0;
         while(s.length() > k){
-            stb.setLength(0);
+            str.setLength(0);
             for(int i = 0; i < s.length(); i += k){
                 ans = 0;
                 for(int j = i; j < i + k && j < s.length(); j++){
                     ans += s.charAt(j) - '0';
                 }
-                stb.append(ans);
+                str.append(ans);
             }
-            s = stb.toString();
+            s = str.toString();
         }
         return s;
     }
